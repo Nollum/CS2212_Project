b4fe5a4fe0421398950a4485465c2cded363b287
Hanniya Zohdi <hzohdi@uwo.ca>
2022-03-31 22:45:40 -0700

    added get and removeBroker methods to interface

--- src/main/java/cryptoTrader/tradingBroker/TradingBrokerListInterface.java
+++ src/main/java/cryptoTrader/tradingBroker/TradingBrokerListInterface.java
@@ -1,7 +1,13 @@
 package cryptoTrader.tradingBroker;
 
+import java.util.ArrayList;
+
 public interface TradingBrokerListInterface {
 
 	public void addBroker(TradingBroker broker);
+	
+	public ArrayList<TradingBroker> getBrokers();
+	
+	public void removeBroker(String brokerName);
 	
 }
